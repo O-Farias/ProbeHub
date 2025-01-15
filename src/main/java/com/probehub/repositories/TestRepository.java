0cf266fc09f3782caf234df92b92936736b83582
Mateus Farias <mateus.f2001@hotmail.com>
2025-01-15 05:25:44 +0000

    chore: remove arquivos não utilizados Test.java e TestRepository.java

--- src/main/java/com/probehub/repositories/TestRepository.java
+++ src/main/java/com/probehub/repositories/TestRepository.java
@@ -1,10 +1,10 @@
 package com.probehub.repositories;
 
-import com.probehub.models.Test;
+import com.probehub.models.ProbeTest;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface TestRepository extends JpaRepository<Test, Long> {
-    
+public interface TestRepository extends JpaRepository<ProbeTest, Long> {
+
 }
